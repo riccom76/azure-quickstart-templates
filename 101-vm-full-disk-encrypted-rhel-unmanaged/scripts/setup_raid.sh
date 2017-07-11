6a4c81629ff9e2df15c42db5abbf7d589f3f5cd0
riccom76 <ricc@microsoft.com>
2017-07-11 17:58:17 +0200

    Update setup_raid.sh

--- 101-vm-full-disk-encrypted-rhel-unmanaged/scripts/setup_raid.sh
+++ 101-vm-full-disk-encrypted-rhel-unmanaged/scripts/setup_raid.sh
@@ -9,7 +9,7 @@ for DISK in $DATADISKS; do
 	DATADISKSFULLNAMES="$DATADISKSFULLNAMES /dev/$DISK"
 done
 
-yum install -y mdadm
+noninteractive apt-get install -y mdadm
 mdadm --create --verbose /dev/md0 --level=0 --raid-devices=2 $DATADISKSFULLNAMES
 mkdir -p /etc/mdadm
 mdadm --detail --scan > /etc/mdadm/mdadm.conf
